664df003ef034b1e72ef397ad0a3c53024162e7b
Samara <Samara@DESKTOP-LJ3S0JA>
2024-02-06 16:39:33 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio 1
+            int N1, N2, Soma;
+            Console.WriteLine("Digite um número");
+            N1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um número");
+            N2 = int.Parse(Console.ReadLine());
+            Soma = N1 + N2;
+            Soma = N1 + N2;
+            Console.WriteLine("A soma é" + Soma);
+
+        }
+    }
+}
