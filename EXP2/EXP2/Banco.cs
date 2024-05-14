885ca4565f1249be290a9f6bb972b94991e45526
Samara Hellen <samara.hellen@yahoo.com.br>
2024-05-14 13:20:12 -0300

    Atualizado

---
+++ EXP2/EXP2/Banco.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class Banco
+{
+	//Atributos
+	private int _idconta;
+	private string _nome;
+	private int _saldo;
+
+	//Método construtor
+	public Banco(int IdConta, string Nome, int Saldo)
+	{
+		_idconta = IdConta;
+		_nome = Nome;
+		_saldo = Saldo;
+	}
+	public Banco() {
+		_idconta = 0;
+		_nome = null;
+		_saldo = 0;
+	}
+	//Get e Set
+	public string Nome
+	{
+		get { return _nome; }
+		set { _nome = value; }
+	}
+
+	public int IdConta
+	{
+		get { return _idconta; }
+	}
+    public void SetIdConta(int novoId)
+    {
+        // Verifique se o ID já foi definido
+        if (_idconta != 0)
+        {
+            Console.WriteLine("O ID da conta não pode ser alterado após a criação.");
+            return;
+        }
+
+        _idconta = novoId;
+    }
+
+    public int Saldo
+	{
+		get { return _saldo; }
+		set { _saldo = value; }
+	}
+
+    //Metodos de Saque e Deposito
+
+    //ToString
+    public override string ToString()
+    {
+        return $"Conta #{_idconta}: Nome: {_nome}, Saldo: {_saldo}";
+    }
+
+
+}
---
+++ EXP2/EXP2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq.Expressions;
+using System.Net.Http.Headers;
+using ExP2; // Adicione esta linha
+namespace ExP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op, id, saldo, i, op2, saldoAtual, deposito;
+            String nome;
+            Banco cliente;
+            cliente = new Banco();
+            do {   
+            Console.WriteLine("Bem-vindo ao sistema do banco GALO");
+            Console.WriteLine("Digite a operação desejada: ");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("1 - Criar conta");
+            Console.WriteLine("2 - Exibir dados");
+            Console.WriteLine("3 - Alterar cadastro");
+            Console.WriteLine("4 - Deposito");
+            Console.WriteLine("5 - Saque");
+            op = int.Parse(Console.ReadLine());
+            
+
+            switch (op){
+                case 1:
+                    Console.WriteLine("Digite o ID da conta (não poderá ser alterado): ");
+                    id = int.Parse(Console.ReadLine());
+                    cliente.SetIdConta(id);
+                    Console.WriteLine("Digite o nome: ");
+                    cliente.Nome= Console.ReadLine();
+                    Console.WriteLine("Deseja declarar um saldo inicial? 1-SIM ou 0-NAO: ");
+                    i= int.Parse(Console.ReadLine());
+                    if (i == 0)
+                    {
+                        cliente.Saldo = 0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Digite o saldo inicial: ");
+                        cliente.Saldo=int.Parse(Console.ReadLine());
+                    }
+                    Console.WriteLine("Cadastro realizado com sucesso!");
+                    break;
+                case 2:
+                    Console.WriteLine(cliente.ToString());
+                    break;
+                case 3:
+                        Console.WriteLine(cliente.ToString());
+                        Console.WriteLine("Digite qual dado deseja alterar!");
+                        Console.WriteLine("1 - Nome");
+                        Console.WriteLine("2 - ID");
+                        op2= int.Parse(Console.ReadLine());
+                        switch (op2)
+                        {
+                            case 1:
+                                Console.WriteLine("Digite o nome: ");
+                                cliente.Nome = Console.ReadLine();
+                                break;
+                            case 2:
+                                Console.WriteLine("ERRO");
+                                break;
+                           
+                        }
+                        Console.WriteLine("Cadastro alterado com sucesso!");
+                        break;
+                case 4:
+                        Console.WriteLine("Digite o valor do depósito: ");
+                        deposito = int.Parse(Console.ReadLine());
+                        cliente.Saldo += deposito;
+                        Console.WriteLine("Depósito realizado com sucesso! Saldo atual: R${0}", cliente.Saldo);
+                        break;
+                case 5:
+                        Console.WriteLine("Digite o valor do saque: ");
+                        int valorSaque = int.Parse(Console.ReadLine());
+                        int taxaSaque = 5;
+                        if (valorSaque + taxaSaque <= cliente.Saldo)
+                        {
+                            cliente.Saldo -= valorSaque + taxaSaque;
+                            Console.WriteLine("Saque realizado com sucesso! Saldo atual: R${0}", cliente.Saldo);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Saldo insuficiente para realizar o saque. Saldo atual: R${0}", cliente.Saldo);
+                        }
+                        break;
+
+
+            }
+            } while (op != 0);
+
+        }
+    }
+}
